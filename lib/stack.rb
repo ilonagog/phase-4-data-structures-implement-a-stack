335f6570cbbacc5f9e23565abb95089a98e7ee95
ilona <gogiashvili.ilona@gmail.com>
2023-09-20 18:20:14 -0400

    commit tree 3d2d2a458d43d6d85064ba4b9e814eb7791436c0

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,17 @@
 # your code here
+class Stack
+    def initialize(limit = nil)
+        @stack = []
+        @limit = limit
+    end
+
+    def  push(character)
+        @stack.push(character)
+    end
+    def pop
+        @stack.pop
+    end
+    def peek
+        @stack.last
+    end
+end
